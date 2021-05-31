56b4dbcfe07cb750a7981a729ceb2c9e4575ad92
mishrasub <subhash06582@gmail.com>
2021-05-31 11:39:34 +0530

    Initial Spring Security without JPA (hard coded user) with UserDeatilService

---
+++ src/main/java/io/javabrains/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package io.javabrains.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(). 
+		   antMatchers("/admin").hasRole("ADMIN").
+		   antMatchers("/user").hasAnyRole("USER", "ADMIN").
+		   antMatchers("/").permitAll().
+		   and().formLogin();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+        .ignoring()
+        .antMatchers("/h2-console/**");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
